36786cd72c13c36e8f378b67430a502cdd0084cc
Anderson <97201511+AndersonGomezM@users.noreply.github.com>
2022-09-28 19:40:12 -0400

    Modificando el ArticuloRepository.

--- app/src/main/java/edu/ucne/parcial1_anderson/repository/ArticuloRepository.kt
+++ app/src/main/java/edu/ucne/parcial1_anderson/repository/ArticuloRepository.kt
@@ -1,5 +1,20 @@
 package edu.ucne.parcial1_anderson.repository
 
-class ArticuloRepository {
+import edu.ucne.parcial1_anderson.data.ArticuloDb
+import edu.ucne.parcial1_anderson.data.entity.ArticuloEntity
+import javax.inject.Inject
 
+class ArticuloRepository @Inject constructor(val db: ArticuloDb){
+
+    suspend fun insertarArticulo(articulo: ArticuloEntity){
+        db.articuloDao.insertarArticulo(articulo)
+    }
+
+    suspend fun modificarArticulo(articulo: ArticuloEntity){
+        db.articuloDao.modificarArticulo(articulo)
+    }
+
+    fun getArticulo(ArticuloId: Int) = db.articuloDao.getArticulo(ArticuloId)
+
+    fun getList() = db.articuloDao.getList()
 }
