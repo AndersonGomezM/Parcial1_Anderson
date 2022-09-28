1e4afb28b828921b1a099cad9ff561d19bce6512
Anderson <97201511+AndersonGomezM@users.noreply.github.com>
2022-09-28 19:09:59 -0400

    Programando el ArticuloDao.

--- app/src/main/java/edu/ucne/parcial1_anderson/data/dao/ArticuloDao.kt
+++ app/src/main/java/edu/ucne/parcial1_anderson/data/dao/ArticuloDao.kt
@@ -1,5 +1,26 @@
 package edu.ucne.parcial1_anderson.data.dao
 
-interface ArticuloDao  {
+import androidx.room.*
+import edu.ucne.parcial1_anderson.data.entity.ArticuloEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ArticuloDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertarArticulo(articulo: ArticuloEntity)
+
+    @Update
+    suspend fun modificarArticulo(articulo: ArticuloEntity)
+
+    @Query("SELECT * " +
+            "FROM Articulos " +
+            "WHERE articuloId = :id " +
+            "LIMIT 1")
+    fun getArticulo(id: Int): Flow<ArticuloEntity>
+
+
+    @Query("SELECT * FROM Articulos")
+    fun getList(): Flow<List <ArticuloEntity>>
 
 }
