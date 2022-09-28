3384cad60ed96e1f16506d561a48e5c98c8b188c
Anderson <97201511+AndersonGomezM@users.noreply.github.com>
2022-09-28 19:57:58 -0400

    Modificando el viewModel de la consulta.

--- app/src/main/java/edu/ucne/parcial1_anderson/ui/articulo_list/ArticuloListViewModel.kt
+++ app/src/main/java/edu/ucne/parcial1_anderson/ui/articulo_list/ArticuloListViewModel.kt
@@ -4,31 +4,33 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import edu.ucne.parcial1_anderson.data.entity.ArticuloEntity
+import edu.ucne.parcial1_anderson.repository.ArticuloRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 data class ArticuloListUiState(
-    val registros: List<ArticuloEntity> = emptyList(),
+    val articulos: List<ArticuloEntity> = emptyList(),
     val text: String = "Meeting"
 )
 
 @HiltViewModel
 class ArticuloListViewModel @Inject constructor(
-    /*val repository: ArticuloRepository*/
+    val repository: ArticuloRepository
 ): ViewModel() {
     private val _uiState = MutableStateFlow(ArticuloListUiState())
     val uiState: StateFlow<ArticuloListUiState> = _uiState.asStateFlow()
 
     init {
         viewModelScope.launch {
-            /*repository.getList().collect {
+            repository.getList().collect { list ->
                 _uiState.update {
-                    it.copy( registros = list )
+                    it.copy( articulos = list )
                 }
-            }*/
+            }
         }
     }
 }
