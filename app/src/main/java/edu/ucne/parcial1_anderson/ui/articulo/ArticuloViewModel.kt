eb3fb277864c526c32805dff1d5df8f48ca0b4b1
Anderson <97201511+AndersonGomezM@users.noreply.github.com>
2022-09-28 20:14:10 -0400

    Modificando el viewModel de Articulo.

--- app/src/main/java/edu/ucne/parcial1_anderson/ui/articulo/ArticuloViewModel.kt
+++ app/src/main/java/edu/ucne/parcial1_anderson/ui/articulo/ArticuloViewModel.kt
@@ -1,5 +1,34 @@
 package edu.ucne.parcial1_anderson.ui.articulo
 
-class ArticuloViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.parcial1_anderson.data.entity.ArticuloEntity
+import edu.ucne.parcial1_anderson.repository.ArticuloRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@HiltViewModel
+class ArticuloViewModel @Inject constructor(
+    val repository: ArticuloRepository
+) : ViewModel() {
+
+    var descripcion by mutableStateOf("")
+    var marca by mutableStateOf("")
+    var existencia by mutableStateOf("")
+
+    fun save(){
+        viewModelScope.launch {
+            repository.insertarArticulo(
+                ArticuloEntity(
+                    descripcion = descripcion,
+                    marca = marca,
+                    existencia = existencia.toDouble()
+                )
+            )
+        }
+    }
 }
